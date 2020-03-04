eb80d5781c901e3bf4899327da385a16dc6b6721
BnaiZhang <best.oneself@foxmail.com>
2020-03-05 00:22:09 +0800

    add js

---
+++ html+css+js/js/showmethod.js
@@ -0,0 +1,11 @@
+// JavaScript 能够以不同方式“显示”数据：
+
+// 使用 window.alert() 写入警告框
+// 使用 document.write() 写入 HTML 输出
+// 使用 innerHTML 写入 HTML 元素
+// 使用 console.log() 写入浏览器控制台
+//window.alert("test");
+a = 5;
+b = 6;
+c = a + b
+document.write(c);
