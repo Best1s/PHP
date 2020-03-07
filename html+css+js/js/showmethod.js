250db34d5e87228df1a2b092e6b7e6121c74a5d2
BaniZhang <987066696@qq.com>
2020-03-07 18:39:27 +0800

    add js err

--- html+css+js/js/showmethod.js
+++ html+css+js/js/showmethod.js
@@ -82,7 +82,7 @@ function err1() {
         adddlert("欢迎您，亲爱的用户！");
     }
     catch (err) {
-        document.getElementById("err").innerHTML = err.message;
+        document.getElementById("err").innerHTML = "err1 function：" + err.message;
     }
 }
 function err2() {
@@ -91,11 +91,38 @@ function err2() {
         if (2 < 3) throw "未定义afda";
     }
     catch (err) {
-        document.getElementById("err2").innerHTML = err;
+        document.getElementById("err2").innerHTML = "err2 function：" + err;
     }
     finally {
-        document.getElementById("err3").innerHTML = "无论 try / catch 结果如何都执行的代码块"
+        document.getElementById("err3").innerHTML = "err2 function：" + "无论 try / catch 结果如何都执行的代码块"
     }
 }
 err1();
 err2();
+
+//JavaScript严格模式
+//通过在脚本或函数的开头添加 "use strict"; 来声明严格模式。指令只能在脚本或函数的开头被识别
+//函数中声明严格模式，拥有局部作用域（只有函数中的代码以严格模式执行）
+//严格模式中不允许使用为未来预留的关键词：
+//implements
+//interface
+//let
+//package
+//private
+//protected
+//public
+//static
+//yield
+
+
+//JavaScript this 关键词指的是它所属的对象。
+//在方法中，this 指的是所有者对象。
+//单独的情况下，this 指的是全局对象。
+//在函数中，this 指的是全局对象。
+//在函数中，严格模式下，this 是 undefined。
+//在事件中，this 指的是接收事件的元素。
+//显式函数绑定
+//call() 和 apply()
+
+//let 关键词声明拥有块作用域的变量。
+//const 定义的变量与 let 变量类似，但不能重新赋值
